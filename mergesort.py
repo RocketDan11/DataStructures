b30fcb1bf3bd927ce301c0d6d58b3696cac73090
Dman <Djolin1@charlotte.edu>
2023-06-30 21:40:06 -0400

    merge sort

---
+++ mergesort.py
@@ -0,0 +1,40 @@
+
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst
+    
+    mid = len(lst) // 2
+    left_half = lst[:mid]
+    right_half = lst[mid:]
+    
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+    
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    
+    return result
+
+# EXAMPLE
+lst = [5, 2, 8, 3, 1, 7]
+sorted_arr = merge_sort(lst)
+print(sorted_arr)
